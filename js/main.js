91028e55e204991bae07da3c474b4f87b70c2f00
ktyler828 <ktyler828@gmail.com>
2021-11-09 18:32:57 -0700

    Basic Map Created
    loaded osm base map and added fire polygons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
 //insert code here!
+var map;
+
+function createMap(){
+
+    map = L.map('map').setView([39.07269613220839, -105.375888968249], 7);
+
+    var openStreetMap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    })
+
+    openStreetMap.addTo(map);
+
+    //call getData function
+    getData(map);
+};
+
+//function to retrieve the data and place it on the map
+function getData(map){
+    //load the data
+    $.getJSON("data/co_fire.json", function(response){
+
+      //create marker options
+      var geojsonMarkerOptions = {
+          radius: 2,
+          fillColor: "magenta",
+          color: "#000",
+          weight: 1,
+          opacity: 1,
+          fillOpacity: 0.8
+      };
+
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+            }}).addTo(map);
+    });
+};
+
+$(document).ready(createMap);
