c5eec05aba5a60022e0cb6fae17c5766f02bc45e
therrmann200 <90002802+therrmann200@users.noreply.github.com>
2021-11-15 18:28:47 -0700

    i dont know whats happening

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //insert code here!
 var map;
 
-function createMap(){
+function createMap() {
 
     map = L.map('map').setView([39.07269613220839, -105.375888968249], 7);
 
@@ -12,30 +12,46 @@ function createMap(){
     openStreetMap.addTo(map);
 
     //call getData function
-    getData(map);
+    Data(map);
 };
 
 //function to retrieve the data and place it on the map
-function getData(map){
+function Data(map) {
     //load the data
-    $.getJSON("data/co_fire.json", function(response){
 
-      //create marker options
-      var geojsonMarkerOptions = {
-          radius: 2,
-          fillColor: "magenta",
-          color: "#000",
-          weight: 1,
-          opacity: 1,
-          fillOpacity: 0.8
-      };
+    $.getJSON("data/co_fire.json", function (response) {
 
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-            }}).addTo(map);
+        //create marker options
+        var geojsonMarkerOptions = {
+            radius: 2,
+            fillColor: "magenta",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+
+        //create a Leaflet GeoJSON layer and add it to the map
+        L.geoJson(response, {
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, geojsonMarkerOptions);
+            }
+        }).addTo(map);
+        var layer = L.leafletGeotiff("data/GeoTIFFproject/y2/Nov_2001.tif",options={band: 0,
+            displayMin: 0,
+            displayMax: 30,
+            name: 'Wind speed',
+            colorScale: 'rainbow',
+            clampLow: false,
+            clampHigh: true,
+            //vector:true,
+            arrowSize: 20,
+        }
+    ).addTo(map);
+
     });
 };
+
+
 
 $(document).ready(createMap);
